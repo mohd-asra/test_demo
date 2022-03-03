46cb34763a08f6605fa1e7e0061434d91beecbbc
mohd-asra <99968685+mohd-asra@users.noreply.github.com>
2022-03-03 22:34:40 +0530

    Update constructorDemo.java

--- constructorDemo.java
+++ constructorDemo.java
@@ -1,21 +1,33 @@
 package constuctor;
-class EmpInfo{
-	int id;
-	String name;
 
-void display() {
-	System.out.println(id+" "+name);
+class StudentInfo{									//parent class
+	int rollNo;
+	String name;
+	StudentInfo(){									// no argument constructor by default initialize variable with zeros and null
+		System.out.println("default constructor is called\n"+ "roll no: " + rollNo +"\n"+ "name: " + name+ "\n");
 	}
 }
 
-public class constructorDemo {
 
-public static void main(String[] args) {
 
-	EmpInfo emp1=new EmpInfo();
-	EmpInfo emp2=new EmpInfo();
+class Student extends StudentInfo{  			 //child class extending parent class
+	int rollNo;
+	String name;		
+	Student(int rollNo , String name){						// parameterized constructor
+		System.out.println("parameterized constructor is called \nroll no: " + (this.rollNo=1));
+		System.out.println("Name: " + (this.name="ASRA"));
 
-	emp1.display();
-	emp2.display();
+	}
+}
+
+
+
+public class Constructors {
+
+	public static void main(String[] args) {
+		
+		//StudentInfo obj = new StudentInfo();   //no need cause -
+		Student obj1 = new Student(1,"ASRA"); //child class constructors also calls the parents class constructor as well
+		
 	}
 }
